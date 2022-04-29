a983274b3a366f673c8a38f976d927558b07d583
DESKTOP-S7AI0RD\ADMIN <ican5357@buyaladdinrnd.com>
2022-04-29 16:52:57 +0900

    4.29 study

---
+++ react-study/src/App.js
@@ -0,0 +1,13 @@
+import CoinConverter from "./coinconverter/CoinConverter";
+import Todo from "./todolist/Todo";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Todo /> */}
+      <CoinConverter />
+    </div>
+  );
+}
+
+export default App;
